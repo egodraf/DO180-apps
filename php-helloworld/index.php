b0cf9306d63759a71d3c57c8044da774000c34cd
egodraf <97973218+egodraf@users.noreply.github.com>
2023-01-12 13:36:58 -0500

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,8 @@
-<?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "A change is comming""\n";
-?>
+<html>
+<head>
+<title>PHP Test</title>
+</head>
+<body>
+<?php echo '<p>Hello World</p>'; ?>
+</body>
+</html>
